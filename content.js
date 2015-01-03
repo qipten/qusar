b462402740784e6c30e413a4a0ac69085d0297c4
qipten <qipten@gmail.com>
2015-01-03 14:35:43 +0400

    Стандарты 0.2 => 0.3 Из контента убрано всё лишнее Согласно стандартам отформатирован манифест Добавлены шаблоны документа в options.html и popup.html
    Signed-off-by: qipten

--- content.js
+++ content.js
@@ -1,14 +1,9 @@
-var adjutant, divNote, isDrag, shiftX, shiftY, window;
+var adjutant, window;
 
-window.console.log('qusar work');
-
-divNote = window.document.createElement('div');
-divNote.className = 'qusar-note';
-
-//window.document.body.appendChild(divNote);
-
-window.document.body.onclick = function() {
-	adjutant.report = {
+window.document.body.onclick = function()
+{
+	adjutant.report =
+	{
 		message: 'click',
 		page: window
 	};
